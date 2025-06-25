7eea2718a1433fc973da5d9835a3c4ac3949318a
VISHAL MR <gokulmr007@gmail.com>
2025-06-25 10:12:29 +0530

    project created

--- CMS/settings.py
+++ CMS/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'receptionist',
     'doctor',
     'labtechnician',
+    'administrator',
 ]
 
 MIDDLEWARE = [
--- cmsbackend/admin.py
+++ cmsbackend/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Staff, Role, Doctor, Specialization
 
-# Register your models here.
+admin.site.register(Staff)
+admin.site.register(Role)
+admin.site.register(Doctor)
+admin.site.register(Specialization)
